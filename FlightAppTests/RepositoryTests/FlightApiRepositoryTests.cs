a418910e1dd386cfa540968f7627ce178db04ed8
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2025-01-29 16:22:47 +0000

    Create test template

---
+++ FlightAppTests/RepositoryTests/FlightApiRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using FlightApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightAppTests.RepositoryTests
+{
+    public class FlightApiRepositoryTests
+    {
+        private FlightApiRepository _repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repository = new FlightApiRepository();
+        }
+    }
+}
