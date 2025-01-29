37f6f6de48cc3b178548aa810b773b0882b6a2b9
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-01-29 16:28:36 +0000

    added automapper

---
+++ FlightApp/Mapper/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FlightApp.Dtos;
+using FlightApp.Models;
+
+namespace FlightApp.Mapper
+{
+    public class MapperProfile:Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Flight, FlightDto>().ReverseMap();
+        }
+    }
+}
