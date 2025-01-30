a1fde40a3a3b0ab8baf5104f26a906641752047e
Charlie Lashmar <charlie@lashmar.com>
2025-01-30 16:25:55 +0000

    created route for search results

---
+++ FlightAppLibrary/Models/SearchModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightAppLibrary.Models
+{
+    public class SearchModel
+    {
+        public string? DepartureIATA { get; set; }
+
+        public string? ArrivalIATA { get; set; }
+    }
+}
