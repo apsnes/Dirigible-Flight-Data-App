79a19ce477b87dc7b62ca9b614c536ac0dbde5be
termgiles <Thomas.giles2222@gmail.com>
2025-02-03 08:36:51 +0000

    Parsing classes for aircraft photos

---
+++ FlightAppLibrary/Models/AircraftPhoto/Photo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FlightAppLibrary.Models.AircraftPhoto
+{
+    public class Photo
+    {
+        public string Id { get; set; }
+        public Thumbnail Thumbnail { get; set; }
+        [JsonPropertyName("thumbnail_large")]
+        public Thumbnail ThumbnailLarge { get; set; }
+        public string Link { get; set; }
+        public string Photographer { get; set; }
+
+    }
+}
