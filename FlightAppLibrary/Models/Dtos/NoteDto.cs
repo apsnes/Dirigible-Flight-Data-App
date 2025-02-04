707f09470816e47c22dc6ce6c601f1736176032e
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2025-02-04 10:15:17 +0000

    Refactor Database

--- FlightAppLibrary/Models/Dtos/NoteDto.cs
+++ FlightAppLibrary/Models/Dtos/NoteDto.cs
@@ -4,11 +4,9 @@ namespace FlightAppLibrary.Models.Dtos
 {
     public class NoteDto
     {
-        public int Id { get; set; }
-        
-        public int UserId { get; set; }
-        
-        public List<FlightNoteDto> FlightNotes { get; set; }
+        public int NoteId { get; set; }      
+        public string UserId { get; set; }
+        public int FlightId { get; set; }
         public DateTime TimeStamp { get; set; }
     }
 }
